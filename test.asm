b1f9dfa9bc838d3b1aa7819e300f6483deb547e2
Sam <90007553+Scherso@users.noreply.github.com>
2023-01-27 18:57:26 +0000

    use correct pointers

--- test.asm
+++ test.asm
@@ -1,7 +1,13 @@
 bits 64
 
-; Simple "Hello World" program which will write with our
-; Type definitions in 'include/efi/efi.asm'.
+; Simple "Hello World!" program which will write with our
+; Type definitions in 'include/efi/efi.asm'. 64
+
+; To produce a '.efi' PIE file
+; nasm -f win64 -o test.obj test.asm
+; lld-link -subsystem:efi_application -entry:_start \ 
+; -dll -nodefaultlib test.obj -out:test.efi
+; From here you can copy to a fat filesystem image.
 
 %include "include/efi/efi.asm"
 
@@ -10,13 +16,13 @@ global _start
 section .text
 
 _start:
-    mov    rax, [rdi + EFI_SYSTEM_TABLE.ConOut]                  ; Get the pointer to the EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
-    mov    rdi, msg                                              ; Set the message to print with our argument being 'rdi'.
-    call   [rax + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]  ; Call the OutputString function of the EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.
+        sub    rsp, [4 * 8]                                          ; Reserving four arguments.
+        ; RDX = EFI_SYSTEM_TABLE
+        mov    rcx, [rdx + EFI_SYSTEM_TABLE.ConOut]                  ; Moving 'ConOut' to our system table structure.
+        lea    rdx, [rel message]                                    ; Loading our string register onto register 'RDX'.
+        call   [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]  ; Calling 'OutputString' by the value of register 'RDX'.
 
-    jmp $                                                        ; Looping forver
+        jmp $                                                        ; Jumping to this instruction, this will allow us to see output.
 
 section .data
-
-msg:
-    db "Hello, World!", 0
+    message dw "Hello, World!",13,10,0
